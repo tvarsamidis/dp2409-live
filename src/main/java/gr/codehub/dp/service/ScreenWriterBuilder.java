e3f393b308bd3eec5b77bc2fa9c63b7a812f82c8
Thomas Varsamidis <t.varsamidis@codehub.gr>
2024-09-20 14:59:25 +0300

    Created ScreenWriter builder

---
+++ src/main/java/gr/codehub/dp/service/ScreenWriterBuilder.java
@@ -0,0 +1,26 @@
+package gr.codehub.dp.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ScreenWriterBuilder {
+    private List<String> lines = new ArrayList<>();
+    private String filename = "*** FILENAME MISSING ****";
+    
+    public ScreenWriterBuilder lines(List<String> lines){
+        this.lines = lines;
+        return this;
+    }
+
+    public ScreenWriterBuilder filename(String filename){
+        this.filename = filename;
+        return this;
+    }
+    
+    public ScreenWriter build() {
+        ScreenWriter writer = new ScreenWriter();
+        writer.setFilename(filename);
+        writer.setLines(lines);
+        return writer;
+    }
+}
