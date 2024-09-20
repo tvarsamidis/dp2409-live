e58e0802d9685c60bbb293ff7fe13f18b3e776a1
Thomas Varsamidis <t.varsamidis@codehub.gr>
2024-09-20 08:47:37 +0300

    Old version

---
+++ src/main/java/gr/codehub/dp/Dp2409Live.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package gr.codehub.dp;
+
+/**
+ *
+ * @author Thomas
+ */
+public class Dp2409Live {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
